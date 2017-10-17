1a51197e6b50f08ea0fd5b564307fc1275dadb35
Maarten Huijsmans <maarten@webmonks.io>
2017-10-17 18:38:24 +0200

    adding rendering

--- deployment/docker-entrypoint.sh
+++ deployment/docker-entrypoint.sh
@@ -11,6 +11,7 @@ Usage: docker run <imagename> COMMAND
 Commands
 pose     : Generate pose predictions
 bodyfit  : Apply bodyfit
+render   : Render a fitted body
 help     : Show this message
 """
 }
@@ -26,13 +27,20 @@ function format_file() {
 # Run
 case "$1" in
     bash)
+        python ${BASE}/setup.py develop > /dev/null 2>&1
         /bin/bash "${@:2}"
     ;;
     pose)
+        python ${BASE}/setup.py develop > /dev/null 2>&1
         python pose/pose.py --use_cpu $(format_file ${@:2})
     ;;
     bodyfit)
+        python ${BASE}/setup.py develop > /dev/null 2>&1
         python 3dfit/bodyfit.py $(format_file ${@:2})
+    ;;
+    render)
+        python ${BASE}/setup.py develop > /dev/null 2>&1
+        python 3dfit/render.py $(format_file ${@:2})
     ;;
     *)
         show_help
