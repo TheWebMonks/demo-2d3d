e8887a2c3b5d6746060e415416402ef4b6e004bd
Maarten Huijsmans <maarten@webmonks.io>
2017-10-17 22:56:41 +0200

    cleaner prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
-curl -O http://files.is.tuebingen.mpg.de/classner/up/models/p91.zip
-mv p91.zip models/
+
+function fetch() {
+  if [ ! -f models/${@:1}.zip ]; then
+    echo "Downloading ${@:1}.zip"
+    curl -O http://files.is.tuebingen.mpg.de/classner/up/models/${@:1}.zip
+    mv ${@:1}.zip models/
+  else
+    echo "Already downloaded ${@:1}.zip"
+  fi
+}
+
+fetch 'p91'
+fetch 's31'
 
 echo "Download SMPL_python_v.1.0.0.zip from http://smpl.is.tue.mpg.de/ and put it in the ./models folder"
